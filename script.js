6c73b4406fbd60d3f3a0186cfe9142ee0d17bb36
Crownkay <adekomsk@gmail.com>
2022-11-02 02:13:25 -0700

    add some changes

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let count = 0;
 
 let timer = false;
 
-function start(){
+function start() {
   timer = true;
   stopwatch();
 }
 
-function reset(){
+function reset() {
   hr = 0;
   min = 0;
   sec = 0;
@@ -24,43 +24,43 @@ function reset(){
   document.getElementById("counts").innerHTML = "00";
 }
 
-function stop(){
+function stop() {
   timer = false;
 }
 
-function stopwatch(){
-  if(timer == true){
-    count = count+1;
+function stopwatch() {
+  if (timer == true) {
+    count = count + 1;
 
-    if(count == 100){
+    if (count == 100) {
       sec = sec + 1;
-      count=0;
+      count = 0;
     }
-    if(sec == 60){
+    if (sec == 60) {
       min = min + 1;
       sec = 0;
     }
-    if(min == 60){
-      hr = hr+1;
+    if (min == 60) {
+      hr = hr + 1;
       min = 0;
       sec = 0;
     }
-    
+
     var hrString = hr;
     var minString = min;
     var secString = sec;
     var countString = count;
 
-    if(hr < 10){
+    if (hr < 10) {
       hrString = "0" + hrString;
     }
-    if(min < 10){
-      minString = '0' + minString;
+    if (min < 10) {
+      minString = "0" + minString;
     }
-    if(sec < 10){
+    if (sec < 10) {
       secString = "0" + secString;
     }
-    if(count < 10){
+    if (count < 10) {
       countString = "0" + countString;
     }
 
@@ -68,6 +68,6 @@ function stopwatch(){
     document.getElementById("minute").innerHTML = minString;
     document.getElementById("seconds").innerHTML = secString;
     document.getElementById("counts").innerHTML = countString;
-    setTimeout("stopwatch()", 10)
+    setTimeout("stopwatch()", 10);
   }
 }
